8e4b43e9e4b089724f38e7e03e0bcad6580b7d3c
Your Name <you@example.com>
2025-06-30 19:10:06 +0530

    lots of real fixes

--- oracles/mv_queries/401_EventPriceBase.sql
+++ oracles/mv_queries/401_EventPriceBase.sql
@@ -6,8 +6,8 @@ SELECT
     aave_ethereum.PriceLatestEventRawNumerator.timestamp AS timestamp,
     aave_ethereum.PriceLatestEventRawNumerator.numerator AS numerator,
     aave_ethereum.PriceLatestEventRawDenominator.denominator AS denominator,
-    aave_ethereum.PriceLatestEventRawMultiplier.multiplier,
-    aave_ethereum.PriceLatestEventRawMaxCap.max_cap,
+    aave_ethereum.PriceLatestEventRawMultiplier.multiplier AS multiplier,
+    aave_ethereum.PriceLatestEventRawMaxCap.max_cap AS max_cap,
     CAST(pow(10, aave_ethereum.LatestAssetSourceTokenMetadata.decimals_places) AS UInt256) AS decimals_places,
     CAST((
         CAST(aave_ethereum.PriceLatestEventRawNumerator.numerator AS Float64)
--- oracles/mv_queries/403_TransactionPriceBase.sql
+++ oracles/mv_queries/403_TransactionPriceBase.sql
@@ -6,8 +6,8 @@ SELECT
     aave_ethereum.PriceLatestTransactionRawNumerator.timestamp AS timestamp,
     aave_ethereum.PriceLatestTransactionRawNumerator.numerator AS numerator,
     aave_ethereum.PriceLatestEventRawDenominator.denominator AS denominator,
-    aave_ethereum.PriceLatestTransactionRawMultiplier.multiplier,
-    aave_ethereum.PriceLatestEventRawMaxCap.max_cap,
+    aave_ethereum.PriceLatestTransactionRawMultiplier.multiplier AS multiplier,
+    aave_ethereum.PriceLatestEventRawMaxCap.max_cap AS max_cap,
     CAST(pow(10, aave_ethereum.LatestAssetSourceTokenMetadata.decimals_places) AS UInt256) AS decimals_places,
     CAST((
         CAST(aave_ethereum.PriceLatestTransactionRawNumerator.numerator AS Float64)
