fead30f26aa9fad80de46b36a8a3fbf4e1f7370b
Your Name <you@example.com>
2025-07-04 01:16:48 +0530

    sme ,re cjamges

--- oracles/mv_queries/800_PriceVerificationRecords.sql
+++ oracles/mv_queries/800_PriceVerificationRecords.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS aave_ethereum.PriceVerificationRecords
     asset String,
     asset_source String,
     name String,
-    type Enum('historical_event', 'historical_transaction', 'predicted_transaction'),
+    type String,
     blockTimestamp DateTime64(6),
     pct_error Float64
 )
