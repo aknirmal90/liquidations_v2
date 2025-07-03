e47f3638be9475f5c0471b86a2c6dfa73385a495
Your Name <you@example.com>
2025-07-04 00:59:17 +0530

    sme ,re cjamges

--- blockchains/mv_queries/210_LatestNetworkBlockInfo.sql
+++ blockchains/mv_queries/210_LatestNetworkBlockInfo.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS aave_ethereum.LatestNetworkBlockInfo
 (
-    network_name String,
+    network_id UInt64,
     latest_block_number UInt64,
     latest_block_timestamp DateTime64(6),
     network_time_for_new_block UInt64
 )
 ENGINE = ReplacingMergeTree(latest_block_timestamp)
-ORDER BY network_name;
+ORDER BY network_id;
