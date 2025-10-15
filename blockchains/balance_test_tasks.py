954fadb96fdb327af5abde44110e5d7c7fb766bd
Nirmal AK <nirmal@merklescience.com>
2025-10-14 22:47:17 -0400

    refactored balances

--- blockchains/balance_test_tasks.py
+++ blockchains/balance_test_tasks.py
@@ -343,7 +343,7 @@ class CompareCollateralBalanceTask(Task):
 
             # Calculate difference in basis points
             difference = abs(ch_balance - rpc_balance)
-            difference_bps = (difference / rpc_balance) * 10000
+            difference_bps = (difference / ch_balance) * 10000
 
             differences_bps.append(difference_bps)
 
